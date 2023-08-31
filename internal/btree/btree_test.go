c61c2f7a0e6ee91692c5f6fc849d3402abd27788
cordea <cordear@outlook.com>
2023-08-31 19:44:39 +0800

    feat: add testify

--- internal/btree/btree_test.go
+++ internal/btree/btree_test.go
@@ -3,6 +3,8 @@ package btree
 import (
 	"godb/internal/pager"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestSerialInsert(t *testing.T) {
@@ -17,13 +19,16 @@ func TestSerialInsert(t *testing.T) {
 	cursor.Insert(2, []byte{0x4, 0x5, 0x6})
 	cursor.Insert(1, []byte{0x1, 0x2, 0x3})
 	cursor.Insert(3, []byte{0x7, 0x8, 0x9})
+	cursor.Insert(9, []byte{0x7, 0x8, 0x9})
+	cursor.Insert(6, []byte{0x7, 0x8, 0x9})
+	cursor.Insert(7, []byte{0x7, 0x8, 0x9})
+	cursor.Insert(11, []byte{0x7, 0x8, 0x9})
+	cursor.Insert(13, []byte{0x13, 0x8, 0x9, 0x13})
+	cursor.Insert(4, []byte{0x4, 0x8, 0x9, 0x15})
 	cellOne := cursor.Mem.GetKthCell(0)
-	cellTwo := cursor.Mem.GetKthCell(1)
-	cellThree := cursor.Mem.GetKthCell(2)
-	t.Log(cellOne.RawData)
-	t.Log(cellOne.Payload)
-	t.Log(cellTwo.RawData)
-	t.Log(cellTwo.Payload)
-	t.Log(cellThree.RawData)
-	t.Log(cellThree.Payload)
+	cellTwo := cursor.Mem.GetKthCell(3)
+	cellThree := cursor.Mem.GetKthCell(8)
+	assert.Equal(t, cellOne.Payload, []byte{0x1, 0x2, 0x3})
+	assert.Equal(t, cellTwo.Payload, []byte{0x4, 0x8, 0x9, 0x15})
+	assert.Equal(t, cellThree.Payload, []byte{0x13, 0x8, 0x9, 0x13})
 }
